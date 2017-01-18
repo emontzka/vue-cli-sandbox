5bf49fd86d05661bbd7a7fca3e7a088ac77bddb7
Eric Montzka <eric@cubicleninjas.com>
2017-01-18 08:19:50 -0600

    initial commit

---
+++ vue-cli/src/components/test.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="test">
+
+    <h1>{{title}}</h1>
+    <ol>
+      <li v-for="item in items">{{item.title}}</li>
+    </ol>
+    <p v-text="user.lastName"></p>
+    <p v-if="showName">{{user.firstName}}</p>
+    <input type="text" v-model="title"><br />
+    <button v-on:click="greet('Hello World')">Say Greeting</button><br>
+    <input type="text" v-on:keyup="pressKey">
+    <hr>
+    <label>First Name: </label><input type="text" v-model="user.firstName">
+    <br>
+    <label>Last Name: </label><input type="text" v-model="user.lastName">
+    <h3>{{fullName}}</h3>   
+    <h2>{{msg}}</h2> 
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'test',
+    props: {
+      msg: {
+        type: String,
+        default: 'Foobar'
+      }
+    },
+    data () {
+      return {
+        title: 'Woo hoo!',
+        items: [
+          {title: 'Item One'},
+          {title: 'Item Two'},
+          {title: 'Item Three'}
+        ],
+        user: {
+          firstName: 'John',
+          lastName: 'Doe'
+        },
+        showName: false
+      }
+    },
+    methods: {
+      greet: function (greeting) {
+        console.log(greeting)
+      },
+      pressKey: function (e) {
+        console.log(e.target.value)
+      }
+    },
+    computed: {
+      fullName: function () {
+        return this.user.firstName + ' ' + this.user.lastName
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
---
+++ vue-cli/src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import vueRouter from 'vue-router'
+import vueResource from 'vue-resource'
+import App from './App'
+
+Vue.use(vueResource)
+Vue.use(vueRouter)
+
+// const router = new vueRouter({
+//   mode: 'history',
+//   base: __dirname,
+//   routes: [
+//   // {path: '/', component: Users}
+//   ]
+
+// })
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
