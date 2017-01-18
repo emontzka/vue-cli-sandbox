8789cb6d868e12dbfaa79ec53468f4f7d430e27d
Eric Montzka <eric@cubicleninjas.com>
2017-01-18 16:55:52 -0600

    add star wars api

--- vue-cli/src/components/users.vue
+++ vue-cli/src/components/users.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="test">
-    <h1>Users</h1>
-    <form v-on:submit="addUser">
+    <h1>People</h1>
+ <!--    <form v-on:submit="addUser">
       <input type="text" v-model="newUser.name" placeholder="Enter Name"> <br>
       <input type="text" v-model="newUser.email" placeholder="Enter Email"><br />
       <input type="submit" value="submit">
-    </form>
+    </form> -->
     
     <ul>
-      <li v-for="user in users">
-        <input type="checkbox" class="toggle" v-model="user.contacted">
-        <span :class="{contacted: user.contacted}">
-          {{user.name}}: {{user.email}}
-        </span>
+      <li v-for="person in people">
+        <!-- <input type="checkbox" class="toggle" v-model="user.contacted"> -->
+        <h1>{{person.name}}</h1>
+        <h2>Traits:</h2>
+        <ul>
+          <li>Born: {{person.birth_year}}</li>
+          <li>Films: {{person.films}}</li>
+
+        </ul>
+       
+          <!-- {{user.name}}: {{user.email}} 
+          <button v-on:click="deleteUser(user)">X</button> -->
+        
       </li>
     </ul>
 
@@ -23,43 +31,41 @@
 
 <script>
   export default {
-    name: 'users',
+    name: 'people',
     data () {
       return {
-        newUser: {},
-        users: [
-          {
-            name: 'John Doe',
-            email: 'jdoe@gmail.com',
-            contacted: false
-          },
-          {
-            name: 'Jane Smith',
-            email: 'jsmith@gmail.com',
-            contacted: false
-          },
-          {
-            name: 'Mary Doe',
-            email: 'mdoe@gmail.com',
-            contacted: false
-          }
-        ]
+        newPerson: {},
+        people: []
 
       }
     },
     methods: {
-      addUser: function (e) {
-        e.preventDefault()
-        this.users.push({
-          name: this.newUser.name,
-          email: this.newUser.email,
-          contacted: false
-        })
-      }
+      // addUser: function (e) {
+      //   e.preventDefault()
+      //   this.users.push({
+      //     name: this.newUser.name,
+      //     email: this.newUser.email,
+      //     contacted: false
+      //   })
+      // }
+      // deleteUser: function (user) {
+      //   this.users.splice(this.users.indexOf(user), 1)
+      // }
+    },
+    created: function () {
+      this.$http.get('http://swapi.co/api/people/')
+      .then(function (response) {
+        console.log(response.data.results)
+        console.log(response.data.results[0].birth_year)
+        this.people = response.data.results
+      })
     }
 
   }
 </script>
 
 <style scoped>
+.contacted {
+  text-decoration: line-through;
+}
 </style>
